aa20c0322ec2d9ed533cbb876c89b052b6dc7858
irbraun <irbraun@iastate.edu>
2019-07-30 13:57:00 -0500

    small fixes

--- scripts/at_example.py
+++ scripts/at_example.py
@@ -13,16 +13,13 @@ import phenolog.related
 
 
 # Read in a dataframe from a TAIR public release tsv containing fields of interest.
-filename = "/Users/irbraun/phenolog/data/Locus_Germplasm_Phenotype_20180702.txt"
+filename = "/Users/irbraun/phenolog/data/tair/Locus_Germplasm_Phenotype_20180702.txt"
 usecols = ["LOCUS_NAME", "GERMPLASM_NAME", "PHENOTYPE", "PUBMED_ID"]
 names = ["locus", "germplasm", "phenotype", "pubmed_id"]
 renamed = {k:v for k,v in zip(usecols,names)}
 df = pd.read_table(filename, usecols=usecols)
 df.rename(columns=renamed, inplace=True)
 df["id"] = [str(i) for i in df.index.values]
-
-
-print(len(df))
 
 
 
@@ -47,21 +44,23 @@ description_dict_expanded = {i:phenolog.nlp.append_related_words(d, phenolog.rel
 
 # Generate annotations and other structures needed for assessing similarity.
 merged_ontology_file = "../ontologies/mo.obo"
-annotations_file = "../data/at_annotations.tsv"
+annotations_file = "../data/annotations/at_annotations.tsv"
 doc2vec_model_file = "../gensim/apnews_dbow/doc2vec.bin"
 
-term_dict = phenolog.ontology.get_reverse_term_dictionary(ontology_obo_file=merged_ontology_file)
-annotations = phenolog.ontology.annotate_with_rabin_karp(object_dict=description_dict, term_dict=term_dict)
+term_dicts = phenolog.ontology.get_term_dictionaries(ontology_obo_file=merged_ontology_file)
+term_dict_fwd = term_dicts[0]
+term_dict_rev = term_dicts[1]
+annotations = phenolog.ontology.annotate_with_rabin_karp(object_dict=description_dict, term_dict=term_dict_rev)
 phenolog.ontology.write_annotations_to_tsv_file(annotations_dict=annotations, annotations_output_file=annotations_file)
 
 
 
 
 # Generate dataframes for each method of assessing similarity between the descriptions.
-print(phenolog.similarity.get_similarity_df_using_ontologies(merged_ontology_file, annotations_file, description_dict).head())
-print(phenolog.similarity.get_similarity_df_using_doc2vec(doc2vec_model_file, description_dict).head())
-print(phenolog.similarity.get_similarity_df_using_bagofwords(description_dict).head())
-print(phenolog.similarity.get_similarity_df_using_setofwords(description_dict).head())
+print(phenolog.similarity.get_similarity_df_using_ontologies(merged_ontology_file, annotations_file, description_dict).head(30))
+print(phenolog.similarity.get_similarity_df_using_doc2vec(doc2vec_model_file, description_dict).head(30))
+print(phenolog.similarity.get_similarity_df_using_bagofwords(description_dict).head(30))
+print(phenolog.similarity.get_similarity_df_using_setofwords(description_dict).head(30))
 
 
 
