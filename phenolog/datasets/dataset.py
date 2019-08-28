0298e693f2a727d35fc02471763742582354fbe5
irbraun <irbraun@iastate.edu>
2019-08-28 12:35:15 -0500

    adding methods for subsampling datasets

--- phenolog/datasets/dataset.py
+++ phenolog/datasets/dataset.py
@@ -31,14 +31,30 @@ class Dataset:
 
 
 	def __init__(self):
-		self.col_names = ["id", "species", "gene_names", "description", "terms", "reference"]
+		self.col_names = ["id", "species", "gene_names", "description", "term_ids", "pmid"]
 		self.col_names_without_id = self.col_names
 		self.col_names_without_id.remove("id")
 		self.df = pd.DataFrame(columns=self.col_names)
 
+	def _reset_ids(self):
+		self.df.id = [str(i) for i in self.df.index.values]
+		self.df = self.df[["id", "species", "gene_names", "description", "term_ids", "pmid"]]
+
+
+
+
 
 
 	def add_data(self, df_newlines):
+		"""
+		Add a dataframe containing information to this dataset. The dataframe provided has to 
+		contain all the columns mentioned in the constructur for this class except for the ID
+		column which is generated once all the data has been added so that the values are always
+		still unique and useful as matrix indices (integers from 0 to n) after any data is added
+		to the dataset.
+		Args:
+		    df_newlines (pandas.DataFrame): The dataframe containing rows to be added.
+		"""
 		df_newlines = df_newlines[self.col_names_without_id]
 		df_newlines["id"] = None
 		df_newlines.fillna("", inplace=True)
@@ -50,21 +66,43 @@ class Dataset:
 
 
 	def randomly_subsample_dataset(self, n, seed):
+		"""Remove all but n randomly sampled points from the dataset.
+		Args:
+		    n (int): The number of datapoints or rows to retain.
+		    seed (int): Seed value for reproducibility of random process.
+		"""
 		self.df = self.df.sample(n=n, random_state=seed)
 		self._reset_ids()
 
 
 
-	def _reset_ids(self):
-		self.df.id = [str(i) for i in self.df.index.values]
-		self.df = self.df[["id", "species", "gene_names", "description", "terms", "evidence", "reference"]]
+	def subsample_has_description(self):
+		"""Remove points that don't have a text description.
+		"""
+		self.df = self.df[self.df["description"] != ""] 
+		self._reset_ids()
+
+
+	def subsample_has_annotation(self):
+		"""Remove points that don't have atleast one ontology term annotation.
+		"""
+		self.df = self.df[self.df["term_ids"] != ""]
+		self._reset_ids()
 
 
 
 
 	
 
+
+
+
+
 	def get_gene_dictionary(self):
+		"""Get a mapping from IDs to gene objects.
+		Returns:
+		    TYPE: Description
+		"""
 		gene_dict = {}
 		for row in self.df.itertuples():
 			delim = "|"
@@ -73,8 +111,27 @@ class Dataset:
 			gene_dict[row.id] = gene_obj
 		return(gene_dict)
 
-	
+
+
+	def get_annotations_dictionary(self):
+		"""Get a mapping from IDs to lists of ontology term IDs.
+		Returns:
+		    TYPE: Description
+		"""
+		annotations_dict = {}
+		for row in self.df.itertuples():
+			delim = "|"
+			term_ids = row.gene_names.split(delim)
+			annotations_dict[row.id] = term_ids
+		return(annotations_dict)
+
+
+
 	def get_description_dictionary(self):
+		"""Get a mapping from IDs to text descriptions.
+		Returns:
+		    TYPE: Description
+		"""
 		description_dict = {identifier:description for (identifier,description) in zip(self.df.id, self.df.description)}
 		return(description_dict)
 
@@ -84,17 +141,27 @@ class Dataset:
 
 
 
-	def set_genes_as_nodes(self):
+
+
+
+	def collapse_by_gene_names(self):
+		"""
+		This method reorganizes the internal dataframe object so that any lines that were referring
+		to the same species and that have an overlap of atleast one in the gene names that were included
+		for those lines are combined. This is slow. All data is retained, so that a union of the gene 
+		names and union of ontology term annotations are found for any lines combined. Text descriptions
+		are combined as well by concatenating using the supporting methods.
+		"""
 
 		# Only perform this operation on slices of the data for one species at a time.
-		# Enforces that genes with the same name across two species have to be two seperate nodes.
+		# This Enforces that genes with the same name across two species have to be two seperate nodes.
 		# Nodes that correspond to different species can never be merged.
 		num_new_rows = 0
 		for species in pd.unique(self.df.species):
 			print(species)
 
 
-			# (1). Create a mapping from gene name strings to row indices where that string is mentioned.
+			# (1) Create a mapping from gene name strings to row indices where that string is mentioned.
 			gene_mention_map = defaultdict(list)
 			for row in self.df.itertuples():
 				if row.species == species:
@@ -104,7 +171,7 @@ class Dataset:
 						gene_mention_map[gene_name].append(row.Index)
 
 
-			# (2). Create a list of sets where all indices in a given set contain synonymous genes (overlap in >=1 name used).
+			# (2) Create a list of sets where all indices in a given set contain synonymous genes (overlap in >=1 name used).
 			list_of_sets_of_row_indices = []
 			for gene_name in gene_mention_map.keys():
 
@@ -123,18 +190,20 @@ class Dataset:
 					list_of_sets_of_row_indices.append(set(gene_mention_map[gene_name]))
 
 
-			# (3). Add rows which contain merged information from multiple rows where the same gene was mentioned.
+			# (3) Add rows which contain merged information from multiple rows where the same gene was mentioned.
 			num_new_rows = num_new_rows + len(list_of_sets_of_row_indices)
 			for set_of_row_indices in list_of_sets_of_row_indices:
 				relevant_rows = self.df.iloc[list(set_of_row_indices)]
 				description = concatenate_descriptions(*relevant_rows.description.tolist())
 				gene_names = concatenate_with_bar_delim(*relevant_rows.gene_names.tolist())
+				term_ids = concatenate_with_bar_delim(*relevant_rows.term_ids.tolist())
 				pmids = concatenate_with_bar_delim(*relevant_rows.pmid.tolist())
 				new_row = {
 					"id":None,
 					"species":species,
-					"description":description,
 					"gene_names":gene_names,
+					"description":description,
+					"term_ids":term_ids,
 					"pmid":pmids,
 				}
 				self.df.append(new_row, ignore_index=True, sort=False)
