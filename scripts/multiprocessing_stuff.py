5af681cf2b49ab7abec4e05f98525154b67db150
irbraun <irbraun@iastate.edu>
2019-09-23 14:49:07 -0500

    updating multiprocessing script

--- scripts/multiprocessing_stuff.py
+++ scripts/multiprocessing_stuff.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+import gensim
 import time
 import multiprocessing as mp
 from sklearn.model_selection import train_test_split
@@ -10,8 +11,11 @@ from sklearn.model_selection import train_test_split
 
 
 sys.path.append("../../oats")
+
+
 # General functions for oats objects and cleaning/preparing text and terms.
-from oats.utils.utils import function_wrapper, to_hms, save_to_pickle, load_from_pickle
+from oats.utils.utils import function_wrapper_with_duration, to_hms, save_to_pickle, load_from_pickle
+from oats.utils.utils import merge_list_dicts
 from oats.datasets.dataset import Dataset
 from oats.datasets.groupings import Groupings
 from oats.nlp.preprocess import get_clean_description
@@ -20,13 +24,12 @@ from oats.annotation.annotation import annotate_using_rabin_karp, annotate_using
 from oats.annotation.annotation import write_annotations_to_tsv_file, read_annotations_from_tsv_file
 
 # Functions for calculating similarity between objects and working with graphs.
-from oats.graphs.similarity import get_similarity_df_using_fastsemsim
-from oats.graphs.similarity import get_similarity_df_using_doc2vec
-from oats.graphs.similarity import get_similarity_df_using_bagofwords
-from oats.graphs.similarity import get_similarity_df_using_setofwords
-from oats.graphs.similarity import get_similarity_df_using_annotations_unweighted_jaccard
-from oats.graphs.similarity import get_similarity_df_using_annotations_weighted_jaccard
-from oats.graphs.data import combine_dfs_with_name_dict, subset_df_with_ids
+from oats.graphs.pairwise import get_edgelist_with_doc2vec
+from oats.graphs.pairwise import get_edgelist_with_bagofwords
+from oats.graphs.pairwise import get_edgelist_with_setofwords
+from oats.graphs.pairwise import get_edgelist_with_annotations
+from oats.graphs.pairwise import merge_edgelists
+from oats.graphs.pairwise import subset_edgelist_with_ids
 
 # Functions for combining different similarity metrics with ml/non-ml models.
 from oats.graphs.models import apply_mean
@@ -40,8 +43,11 @@ from oats.graphs.models import apply_random_forest_model
 # Functions for evaluting graphs based on some objective functions.
 from oats.objectives.functions import classification
 from oats.objectives.functions import consistency_index
-from oats.graphs.graph import Graph
+from oats.graphs.indexed import IndexedGraph
 from oats.objectives.functions import pr
+
+
+
 
 
 
@@ -52,8 +58,9 @@ pd.set_option('display.multi_sparse', False)
 
 # Read in the text descriptions and associated genetic data.
 dataset = Dataset()
+dataset.add_data(pd.read_csv("../data/reshaped_files/all_pppn_gene_textlong.csv", lineterminator="\n"))
 dataset.add_data(pd.read_csv("../data/reshaped_files/ath_tair_gene_text.csv", lineterminator="\n"))
-dataset.add_data(pd.read_csv("../data/reshaped_files/ath_tair_gene_annot_go.csv", lineterminator="\n"))
+dataset.describe()
 
 # Filtering the data that was available from those files.
 dataset.collapse_by_first_gene_name()
@@ -61,12 +68,19 @@ dataset.filter_has_description()
 dataset.filter_has_annotation()
 
 # Randomly subsampling the data.
-dataset.filter_random_k(k=20, seed=78263)
+dataset.describe()
+dataset.filter_random_k(k=1000, seed=78263)
+
+
+
 
 # Get dictionaries mapping IDs to text descriptions or genes.
 descriptions = dataset.get_description_dictionary()
 descriptions = {i:get_clean_description(d) for (i,d) in descriptions.items()}
 genes = dataset.get_gene_dictionary()
+
+
+
 
 # Create/read in the different objects for organizing gene groupings.
 groupings_kegg = load_from_pickle(path="../data/pickles/kegg_pathways.pickle")
@@ -74,12 +88,11 @@ groupings_pmn = load_from_pickle(path="../data/pickles/pmn_pathways.pickle")
 groupings_subset = load_from_pickle(path="../data/pickles/lloyd_subsets.pickle")
 groupings_class = load_from_pickle(path="../data/pickles/lloyd_classes.pickle")
 
-# Get mappings between object IDs and pathways IDs and the reverse.
-id_to_pathway_ids = {}
-id_to_pathway_ids.update(groupings_kegg.get_forward_dict(genes))
-id_to_pathway_ids.update(groupings_pmn.get_forward_dict(genes))
-id_to_pathway_ids.update(groupings_subset.get_forward_dict(genes))
 
+# Get mappings between object IDs and lists of pathways IDs.
+id_to_pathway_ids = merge_list_dicts(groupings_kegg.get_forward_dict(genes), 
+	groupings_pmn.get_forward_dict(genes), 
+	groupings_subset.get_forward_dict(genes))
 
 
 
@@ -87,8 +100,9 @@ id_to_pathway_ids.update(groupings_subset.get_forward_dict(genes))
 merged_ontology_file = "../ontologies/mo.obo"
 annotations_file = "../data/scratch/annot.tsv"
 doc2vec_model_file = "../gensim/enwiki_dbow/doc2vec.bin"
+doc2vec_model = gensim.models.Doc2Vec.load(doc2vec_model_file)
 mo = Ontology(merged_ontology_file)
-annotations = annotate_using_rabin_karp(object_dict=descriptions, ontology=mo)
+annotations = dataset.get_annotations_dictionary()
 write_annotations_to_tsv_file(annotations_dict=annotations, annotations_output_path=annotations_file)
 
 
@@ -97,35 +111,44 @@ write_annotations_to_tsv_file(annotations_dict=annotations, annotations_output_p
 
 
 
+# Single-threaded approach.
+dfs = []
+dfs.append(get_edgelist_with_doc2vec(doc2vec_model, descriptions))
+dfs.append(get_edgelist_with_setofwords(descriptions))
+dfs.append(get_edgelist_with_bagofwords(descriptions))
+dfs.append(get_edgelist_with_annotations(annotations, mo))
+
+names = ["doc2vec", "bagofwords", "setofwords", "ontology"]
+name_to_df_mapping = {name:df for (name,df) in zip(names,dfs)}
+df = merge_edgelists(name_to_df_mapping, default_value=0.000)
 
 
 
 
 
-# Setup for creating the pairwise similarity matrices using seperate cores.
+# Muliprocessing approach, with check for whether there is any savings.
+'''
+
 # The key is the function to use as a variable, and the values are the arguments to unpack as a list.
 functions_and_args = {
-	get_similarity_df_using_fastsemsim:[merged_ontology_file, annotations_file, descriptions, True],
-	get_similarity_df_using_doc2vec:[doc2vec_model_file, descriptions, True],
-	get_similarity_df_using_bagofwords:[descriptions, True],
-	get_similarity_df_using_setofwords:[descriptions, True],
-	get_similarity_df_using_annotations_unweighted_jaccard:[annotations, mo, True],
-	get_similarity_df_using_annotations_weighted_jaccard:[annotations, mo, True]}
+	get_edgelist_with_doc2vec:[doc2vec_model, descriptions],
+	get_edgelist_with_bagofwords:[descriptions],
+	get_edgelist_with_setofwords:[descriptions],
+	get_edgelist_with_annotations:[annotations, mo]}
 
 # Use parallel processing the build all the similarity matrices.
 start_time_mp = time.perf_counter()
 pool = mp.Pool(mp.cpu_count())
-results = [pool.apply_async(function_wrapper, args=(function, args)) for (function, args) in functions_and_args.items()]
+results = [pool.apply_async(function_wrapper_with_duration, args=(function, args)) for (function, args) in functions_and_args.items()]
 results = [result.get() for result in results]
 pool.close()
 pool.join()    
 total_time_mp = time.perf_counter()-start_time_mp
 
 # Create a mapping between method names and the similarity matrices that were generated.
-names = ["ontology", "doc2vec", "bagofwords", "setofwords", "onto_unwt", "onto_wt"]
+names = ["doc2vec", "bagofwords", "setofwords", "ontology"]
 name_to_df_mapping = {name:result[0] for (name,result) in zip(names,results)}
-df = combine_dfs_with_name_dict(name_to_df_mapping)
-
+df = merge_edgelists(name_to_df_mapping, default_value=0.000)
 
 
 # Look at how long it took to build each pairwise similarity matrix.
@@ -140,10 +163,7 @@ print("-----------------------------------------------------------------")
 print("{:15} {}".format("total", to_hms(sum(durations))))
 print("{:15} {} ({:.2%} of single thread time)".format("multiprocess", to_hms(total_time_mp), savings))
 print("\n\n")
-
-
-
-
+'''
 
 
 
@@ -161,8 +181,8 @@ print("\n\n")
 
 # Get the IDs of objects in the dataset that are mapped to atleast one group.
 ids_with_grouping_info = [identifier for (identifier,group_list) in id_to_pathway_ids.items() if len(group_list)>0]
-df_subset = subset_df_with_ids(df, ids_with_grouping_info)
-df_train, df_test = train_test_split(df_subset, test_size=0.2)
+df_subset = subset_edgelist_with_ids(df, ids_with_grouping_info)
+df_train, df_test = train_test_split(df_subset, test_size=0.95)
 print("The size of the training set is {}".format(len(df_train)))
 print("The size of the testing set is {}".format(len(df_test)))
 
@@ -174,14 +194,8 @@ model = train_random_forest_model(df=df_train, predictor_columns=names, target_c
 df_test.loc[:,"ranforest"] = apply_random_forest_model(df=df_test, predictor_columns=names, model=model)
 
 
-
-
-
-
-
-
 # Create graph objects using different similarity measures. Get values of objective functions.
-graphs = [Graph(df=df, value=name) for name in names]
+graphs = [IndexedGraph(df=df, value=name) for name in names]
 results = {name:classification(graph=graph, id_to_labels=id_to_pathway_ids) for (name,graph) in zip(names,graphs)}
 cimaps = {name:consistency_index(graph=graph, id_to_labels=id_to_pathway_ids) for (name,graph) in zip(names,graphs)}
 print(pd.DataFrame(cimaps))
