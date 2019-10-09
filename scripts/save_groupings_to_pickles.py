f151d9dc0bd1eead45af50cb29bd00b6e0beadc1
irbraun <irbraun@iastate.edu>
2019-10-09 16:10:58 -0500

    updating notebooks

--- scripts/save_groupings_to_pickles.py
+++ scripts/save_groupings_to_pickles.py
@@ -14,12 +14,12 @@ from oats.nlp.preprocess import other_delim_to_bar_delim, concatenate_with_bar_d
 
 # Mapping between species codes and relevant files, used in searching pathway databases.
 species_dict = {
-    "ath":"../data/pathway_related_files/plantcyc/aracyc_pathways.20180702", 
-    "zma":"../data/pathway_related_files/plantcyc/corncyc_pathways.20180702", 
-    "mtr":"../data/pathway_related_files/plantcyc/mtruncatulacyc_pathways.20180702", 
-    "osa":"../data/pathway_related_files/plantcyc/oryzacyc_pathways.20180702", 
-    "gmx":"../data/pathway_related_files/plantcyc/soycyc_pathways.20180702",
-    "sly":"../data/pathway_related_files/plantcyc/tomatocyc_pathways.20180702"}
+    "ath":"../data/group_related_files/pmn/aracyc_pathways.20180702", 
+    "zma":"../data/group_related_files/pmn/corncyc_pathways.20180702", 
+    "mtr":"../data/group_related_files/pmn/mtruncatulacyc_pathways.20180702", 
+    "osa":"../data/group_related_files/pmn/oryzacyc_pathways.20180702", 
+    "gmx":"../data/group_related_files/pmn/soycyc_pathways.20180702",
+    "sly":"../data/group_related_files/pmn/tomatocyc_pathways.20180702"}
 
 
 # Create and save the pathways object using PMN.
@@ -29,7 +29,7 @@ save_to_pickle(obj=pathways, path="../data/pickles/pmn_pathways.pickle")
 
 
 # Create and save a pathways object using KEGG.
-pathways = Groupings(species_dict, source="kegg")
+#pathways = Groupings(species_dict, source="kegg")
 pathways.describe()
 save_to_pickle(obj=pathways, path="../data/pickles/kegg_pathways.pickle")
 
@@ -38,7 +38,7 @@ save_to_pickle(obj=pathways, path="../data/pickles/kegg_pathways.pickle")
 
 
 # Some preprocessing necessary for the Lloyd three-level (group, class, subset) functional hierarchy dataset.
-df = pd.read_csv("../data/pathway_related_files/lloyd/lloyd_gene_to_function_irb_cleaned.csv")
+df = pd.read_csv("../data/group_related_files/lloyd/lloyd_gene_to_function_irb_cleaned.csv")
 df.fillna("", inplace=True)
 df["Alias Symbols"] = df["Alias Symbols"].apply(lambda x: other_delim_to_bar_delim(string=x, delim=";"))
 df["gene_names"] = np.vectorize(concatenate_with_bar_delim)(df["Locus"], df["Gene Symbol"], df["Alias Symbols"], df["Full Gene Name"])
@@ -52,19 +52,22 @@ df_subset.columns = ["group_id", "gene_names"]
 df_subset["group_id"] = df_subset["group_id"].apply(lambda x: x.replace("W:", "").replace("S:",""))
 df_subset["group_id"] = df_subset["group_id"].apply(lambda x: other_delim_to_bar_delim(string=x, delim=","))
 df_subset.to_csv("../data/scratch/arabidopsis_subsets.csv", index=False)
-
+# Provide a mapping from subset or class IDs to the longer names that define them.
+df = pd.read_csv("../data/group_related_files/lloyd/lloyd_function_hierarchy_irb_cleaned.csv")
+subset_id_to_name_dict = {row[5]:row[7] for row in df.itertuples()}
+class_id_to_name_dict = {row[3]:row[4] for row in df.itertuples()}
+print(class_id_to_name_dict)
 
 
 
 # Create the save the grouping object using Lloyd function hierarchy dataset of subsets.
 species_dict = {"ath":"../data/scratch/arabidopsis_subsets.csv"}
-subsets = Groupings(species_dict, source="csv")
+subsets = Groupings(species_dict, source="csv", name_mapping=subset_id_to_name_dict)
 subsets.describe()
 save_to_pickle(obj=subsets, path="../data/pickles/lloyd_subsets.pickle")
 
-
 # Create the save the grouping object using Lloyd function hierarchy dataset of classes.
 species_dict = {"ath":"../data/scratch/arabidopsis_classes.csv"}
-classes = Groupings(species_dict, source="csv")
+classes = Groupings(species_dict, source="csv", name_mapping=class_id_to_name_dict)
 classes.describe()
 save_to_pickle(obj=classes, path="../data/pickles/lloyd_classes.pickle")
